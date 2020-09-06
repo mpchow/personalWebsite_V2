8af83cb34d4090d244a49d7faa1a6bf7cbd92600
mpchow <mattchow918@gmail.com>
2020-09-06 12:35:37 -0700

    Created toggle

---
+++ src/components/Toggle/toggle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './toggle.css';
+
+class Toggle extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <div>
+         </div>
+      );
+   }
+}
+
+export default Toggle;
