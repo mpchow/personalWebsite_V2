af7286a8005f245f09dacdff80ab7b27bc2709f2
mpchow <mattchow918@gmail.com>
2020-09-06 12:31:40 -0700

    Restructure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 import Experience from '../Experience/experience';
-import Contact from '../Contact/contact';
 import Intro from '../Intro/intro';
 import Projects from '../Projects/projects';
 import About from '../About/about';
@@ -10,7 +9,6 @@ import About from '../About/about';
 function App() {
   return (
     <div className="App">
-      <Contact />
       <Intro/>
       <About />
       <Experience />
