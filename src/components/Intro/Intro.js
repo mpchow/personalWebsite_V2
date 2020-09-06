478daa9923918174cea2994b875c089db9124096
mpchow <mattchow918@gmail.com>
2020-09-06 12:35:04 -0700

    Format intro page

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './intro.css';
 
+import Toggle from '../Toggle/toggle';
+import Contact from '../Contact/contact';
+
 class Intro extends React.Component {
    constructor(props) {
       super(props);
@@ -8,7 +11,14 @@ class Intro extends React.Component {
 
    render() {
       return (
-         <div>
+         <div className="intro">
+            <Toggle />
+            <div className="intro-text">
+            <h1>Hi, I'm <br/> Matthew Chow 👋🏻</h1>
+            <h2>Software Developer Intern @ SAP <br/> Computer Engineering Student @ UBC</h2>
+
+            </div>
+            <Contact />
          </div>
       );
    }
