96604766b321c0d83ac2ab846f371493e6c245b5
mpchow <mattchow918@gmail.com>
2020-09-10 11:27:04 -0700

    Fixed position on screen:

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './contact.css';
 
-class Contact extends React.Component {
-   constructor(props) {
-      super(props);
-   }
 
+class Contact extends React.Component { 
    render() {
       return (
          <div className="contact">
-            <a>Resume</a> <br/>
+            <a id="resumeLink">Resume</a> <br/>
             <a id="emailLink" href="mailto:mattchow918@gmail.com" target="_blank">Email</a> <br/>
             <a id="githubLink" href="https://github.com/mpchow" target="_blank">Github</a> <br/>
             <a id="linkedInLink" href="https://linkedin.com/in/matthewpchow" target="_blank">LinkedIn</a> <br/>
