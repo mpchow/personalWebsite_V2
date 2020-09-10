949a167447da887eb9b340f9680371c083db9c31
mpchow <mattchow918@gmail.com>
2020-09-10 11:14:09 -0700

    Baseline about section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './about.css';
 
+import memoji from '../../images/memoji.png';
+
 class About extends React.Component {
    constructor(props) {
       super(props);
@@ -9,6 +11,32 @@ class About extends React.Component {
    render() {
       return (
          <div className="about">
+            <div className="about-box">
+               <div className="about-img">
+                  <img src={memoji} />
+               </div>
+               <div className="about-description">
+                  <h1>About Me</h1>
+                  <p>Hello, I'm Matthew, a Software Engineer based in Vancouver.<br/> <br/> I'm studying Computer Engineering at The University of British Columbia and a Software Developer Intern at SAP.<br/> <br/>
+                  I've always been fascinated by technology and I want be a part of developing the software that continues to change human nature. I'm still finding my way around the industry but I have an interest in Backend Development and Embedded Systems and I have picked up some Frontend skills along the way.<br/> <br/> 
+                  Here are a few tools that I have been working with.  <br/> <br/>
+                  </p>
+
+                  <div className="about-skills">
+                     <p>
+                        » Go <br/>
+                        » React <br/> 
+                        » JavaScript <br/>
+                     </p>
+                     <p>
+                        » Java <br/> 
+                        » C++ <br/>
+                        » HTML/CSS <br/>
+                     </p>
+
+                  </div>
+               </div>
+            </div>
          </div>
       );
    }
