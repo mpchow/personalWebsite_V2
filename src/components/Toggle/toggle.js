6c72e4c756ee9e4da97e085522a491157b7bf055
mpchow <mattchow918@gmail.com>
2020-09-13 21:27:07 -0700

    Basic toggle shape

--- src/components/Toggle/toggle.js
+++ src/components/Toggle/toggle.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './toggle.css';
 
 class Toggle extends React.Component {
+   counter = 0;
+
+
    constructor(props) {
       super(props);
    }
 
    render() {
       return (
-         <div>
+         <div className="toggle">
+            <label id="switch" className="switch" onClick={() => {
+            }}>
+               <input type="checkbox"/>
+               <span className="slider round"></span>
+            </label>
          </div>
       );
    }
