eacd06c488ec6291943248ae27e25fd5f43b33c4
mpchow <mattchow918@gmail.com>
2020-09-13 23:21:32 -0700

    Fixing warnings

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -6,7 +6,7 @@ class Contact extends React.Component {
    render() {
       return (
          <div className="contact">
-            <a id="resumeLink">Resume</a> <br/>
+            <a id="resumeLink" href="https://matthewchow.me">Resume</a> <br/>
             <a id="emailLink" href="mailto:mattchow918@gmail.com" target="_blank" rel="noopener noreferrer">Email</a> <br/>
             <a id="githubLink" href="https://github.com/mpchow" target="_blank" rel="noopener noreferrer">Github</a> <br/>
             <a id="linkedInLink" href="https://linkedin.com/in/matthewpchow" target="_blank" rel="noopener noreferrer">LinkedIn</a> <br/>
