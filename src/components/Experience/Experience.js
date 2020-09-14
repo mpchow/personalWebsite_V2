ee0fd33266b04d95d8040a320a87caf4d2ca22ac
mpchow <mattchow918@gmail.com>
2020-09-13 21:26:28 -0700

    Initial experience section

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,11 +4,54 @@ import './experience.css';
 class Experience extends React.Component {
    constructor(props) {
       super(props);
+      this.state = {experience: "SAP"};
+   }
+
+   selectExperience(company) {
+      this.setState({experience: company});
    }
 
    render() {
       return (
-         <div>
+         <div className="experience">
+               <div className="experience-box">
+                  <div className="experience-titles">
+                     <div className="experience-title" onClick={() => {
+                        this.selectExperience("SAP");
+                     }} >
+                        SAP
+                     </div>
+
+                     <div className="experience-title" onClick={() => {
+                        this.selectExperience("Perk Labs");
+                     }}>
+                        Perk Labs
+                     </div>
+
+                     <div className="experience-title" onClick={() => {
+                        this.selectExperience("UBC Solar");
+                     }}>
+                        UBC Solar
+                     </div>
+                  </div>
+                  <div className="experience-description">
+                     <h1>Experience</h1>
+                     {this.state.experience === "SAP" && <p>
+                     » Developing infrastructure for SAP HANA with Python and Golang
+                        </p>}
+                     
+                     {this.state.experience === "Perk Labs" &&  <p>
+                     » Reduced the development team’s workload by developing UX features with React Native <br/> <br/>
+                     » Strengthened UI/UX design by identifying pain points in transaction flows and suggesting readability improvements based on user centered design <br/> <br/>
+                     » Delivered an optimal experience for 5000+ users through regression, integration, and unit tests <br/>
+                        </p>}
+
+                     {this.state.experience === "UBC Solar" && <p>
+                     » Designed a web app with HTML, CSS, and JavaScript to deliver telemetry data from a solar car for strategical analysis during competition <br/> <br/>
+                     » Expanded the user base by creating a scalable frontend design
+                        </p>}
+                  </div>
+               </div>
          </div>
       );
    }
