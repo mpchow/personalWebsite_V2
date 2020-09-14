4dd6f9207c375b7911af1e9fed5f8618616d3995
mpchow <mattchow918@gmail.com>
2020-09-14 00:12:12 -0700

    Added more structure

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './projects.css';
 
+import Project from '../Project/project';
+
 class Projects extends React.Component {
-   // constructor(props) {
-   //    super(props);
-   // }
+   constructor(props) {
+      super(props);
+      this.state = {projectsList: [{title: "CurrenSee", description: "", link: ""}, {title: "FOEX", description: "", link: ""}, {title: "", description: "", link: ""}, {title: "", description: "", link: ""}]}
+   }
 
    render() {
       return (
          <div className="projects">
             <div className="projects-box">
-               
+               <h1 className="projects-title">Projects</h1>
+
+               {this.state.projectsList.map( project => <Project title={project.title} />)}
+
+
+               <div className="projects-archive">
+                  <h1 className="projects-title">Archive</h1>
+               </div>
             </div>
+
+
          </div>
       );
    }
