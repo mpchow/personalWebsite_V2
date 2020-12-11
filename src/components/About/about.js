216159b9e1674c6bacfc12d4313d2cb8e00ba2b5
mpchow <mattchow918@gmail.com>
2020-12-11 01:21:51 -0800

    Header change

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -11,8 +11,9 @@ class About extends React.Component {
                <div className="about-img">
                   <img src={memoji} alt=""/>
                </div>
-               <div className="about-description">
+               <div className="about-content">
                   <h1>About Me</h1>
+                  <div className="about-description">
                   <p>Hello, I'm Matthew, a Software Engineer based in Vancouver.<br/> <br/> I'm studying Computer Engineering at The University of British Columbia and I'm currently a Software Developer Intern at SAP.<br/> <br/>
                   I've always been fascinated by technology and I want be a part of developing the software that continues to change human nature. I'm still finding my way around the industry but I have an interest in Backend Development and Embedded Systems and I have picked up some Frontend skills along the way.<br/> <br/> 
                   Here are a few tools that I have been working with.  <br/> <br/>
@@ -31,6 +32,7 @@ class About extends React.Component {
                      </p>
 
                   </div>
+               </div>
                </div>
             </div>
          </div>
