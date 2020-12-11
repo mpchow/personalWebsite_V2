84d2ed4cd5d9d61e0cb8bd30886982c50965ccb1
mpchow <mattchow918@gmail.com>
2020-12-10 23:39:57 -0800

    cleaning up about section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -13,8 +13,7 @@ class About extends React.Component {
                </div>
                <div className="about-description">
                   <h1>About Me</h1>
-                  <div className="about-text">
-                  <p>Hello, I'm Matthew, a Software Engineer based in Vancouver.<br/> <br/> I'm studying Computer Engineering at The University of British Columbia and a Software Developer Intern at SAP.<br/> <br/>
+                  <p>Hello, I'm Matthew, a Software Engineer based in Vancouver.<br/> <br/> I'm studying Computer Engineering at The University of British Columbia and I'm currently a Software Developer Intern at SAP.<br/> <br/>
                   I've always been fascinated by technology and I want be a part of developing the software that continues to change human nature. I'm still finding my way around the industry but I have an interest in Backend Development and Embedded Systems and I have picked up some Frontend skills along the way.<br/> <br/> 
                   Here are a few tools that I have been working with.  <br/> <br/>
                   </p>
@@ -31,7 +30,6 @@ class About extends React.Component {
                         » HTML/CSS <br/>
                      </p>
 
-                  </div>
                   </div>
                </div>
             </div>
