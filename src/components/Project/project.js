98137570ae3a066bb0456f06e1acf1b5f361d47e
mpchow <mattchow918@gmail.com>
2020-12-10 23:59:55 -0800

    Clean up prop handling

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -12,11 +12,11 @@ class Project extends React.Component {
       return (
          <div className="project-box">
             <div className="project-description">
-               <h1>{this.props.title}</h1>
-               <p>{this.props.description}</p>
+               <h1>{this.props.details.title}</h1>
+               <p>{this.props.details.description}</p>
             </div>
             <div className="project-image">
-               <img src={this.props.image} alt=""/>
+               <img src={this.props.details.image} alt=""/>
             </div>
          </div>
       );
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -9,7 +9,14 @@ import CurrenSee from '../../images/CurrenSee.png';
 class Projects extends React.Component {
    constructor(props) {
       super(props);
-      this.state = {projectsList: [{title: "CurrenSee", description: "» Visualized past currency data to predict future trends by dynamically updating trend lines against a user inputted base currency with React and Chart.js", image: CurrenSee, link: "", github: ""}, {title: "FOEX", description: "", image: "", link: ""}]};
+      this.state = {
+         projectsList: [
+            {title: "pilldex", description: "", image: "", link: null, github: ""},
+            {title: "CurrenSee", description: "Visualized past currency data to predict future trends by dynamically updating trend lines against a user inputted base currency with React and Chart.js", image: CurrenSee, link: "", github: ""},
+            {title: "FOEX", description: "", image: "", link: "", github: ""},
+         ],
+         projectsArchive: []
+      };
    }
 
    render() {
@@ -18,14 +25,14 @@ class Projects extends React.Component {
             <div className="projects-box">
                <h1 className="projects-title">Projects</h1>
 
-               {this.state.projectsList.map( project => <Project title={project.title} description={project.description} image={project.image}/>)}
-{/* 
+               {this.state.projectsList.map( project => <Project details={project}/>)}
+            </div>
+            
+            {/* 
 
                <div className="projects-archive">
                   <h1 className="projects-title">Archive</h1>
                </div> */}
-            </div>
-
 
          </div>
       );
