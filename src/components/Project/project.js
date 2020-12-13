932e0461535a950adf4bbd1ee447b118eccf27ff
mpchow <mattchow918@gmail.com>
2020-12-13 02:33:12 -0800

    More stylistic changes and adding project details

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -12,11 +12,19 @@ class Project extends React.Component {
       return (
          <div className="project-box">
             <div className="project-description">
-               <h1>{this.props.details.title}</h1>
+               <div className="project-header">
+                  <h1 id="project-title">{this.props.details.title} 
+                  </h1>
+                  <div className="project-links">
+                     {this.props.details.link && <a href={this.props.details.link} target="_blank" rel="noopener noreferrer">Link </a>}
+                     {this.props.details.github && <a href={this.props.details.github} target="_blank" rel="noopener noreferrer">Github</a>}
+                  </div>
+
+               </div>
                <p>{this.props.details.description}</p>
             </div>
             <div className="project-image">
-               <img src={this.props.details.image} alt=""/>
+               <img className={this.props.details.portrait ? "project-image-portrait" : "project-image-landscape"} src={this.props.details.image} alt=""/>
             </div>
          </div>
       );
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -4,6 +4,8 @@ import './projects.css';
 import Project from '../Project/project';
 
 import CurrenSee from '../../images/CurrenSee.png';
+import pilldex from '../../images/pilldex.png';
+import FOEX from '../../images/FOEX.png';
 
 
 class Projects extends React.Component {
@@ -11,11 +13,12 @@ class Projects extends React.Component {
       super(props);
       this.state = {
          projectsList: [
-            {title: "pilldex", description: "", image: "", link: null, github: ""},
-            {title: "CurrenSee", description: "Visualized past currency data to predict future trends by dynamically updating trend lines against a user inputted base currency with React and Chart.js", image: CurrenSee, link: "", github: ""},
-            {title: "FOEX", description: "", image: "", link: "", github: ""},
+            {title: "pilldex", description: "", image: pilldex, github: "https://github.com/mpchow/pilldex", portrait: true},
+            {title: "CurrenSee", description: "Visualized past currency data to predict future trends by dynamically updating trend lines against a user inputted base currency with React and Chart.js", image: CurrenSee, link: "https://confident-saha-dc0127.netlify.app", github: "https://github.com/mpchow/CurrenSee", portrait: false},
+            {title: "FOEX", description: "", image: FOEX, github: "https://github.com/mpchow/FOEX_nwHacks2020", portrait: true},
          ],
-         projectsArchive: []
+         projectsArchive: [
+         ]
       };
    }
 
@@ -23,7 +26,7 @@ class Projects extends React.Component {
       return (
          <div className="projects">
             <div className="projects-box">
-               <h1 className="projects-title">Projects</h1>
+               <h1 className="projects-title">Projects</h1> <h1 className="projects-archive">Archive</h1>
 
                {this.state.projectsList.map( project => <Project details={project}/>)}
             </div>
