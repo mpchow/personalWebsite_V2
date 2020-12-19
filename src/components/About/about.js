30a9788f6bb14e51ba9adad3268a3743aa53f3b8
mpchow <mattchow918@gmail.com>
2020-12-19 02:22:21 -0800

    Initial about section redesign

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -4,6 +4,18 @@ import './about.css';
 import memoji from '../../images/memoji.png';
 
 class About extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         aboutSelect: "Intro"
+      }
+   }
+
+   setView(introType) {
+      this.setState({aboutSelect: introType});
+   }
+
+
    render() {
       return (
          <div className="about">
@@ -12,13 +24,21 @@ class About extends React.Component {
                   <img src={memoji} alt=""/>
                </div>
                <div className="about-content">
-                  <h1>About Me</h1>
-                  <div className="about-description">
+                  <div className="about-header">
+                     <h1>About Me</h1>
+                     <div className="about-category">
+                        <p className={this.state.aboutSelect === "Intro" ? "about-category-selected" : null} onClick={() => {this.setView("Intro")}} >Intro</p>
+                        <p className={this.state.aboutSelect === "Skills" ? "about-category-selected" : null} onClick={() => {this.setView("Skills")}} >Skills</p>
+                     </div>
+                  </div>
+                  {this.state.aboutSelect === "Intro" && <div className="about-description">
                   <p>Hello, I'm Matthew, a Software Engineer based in Vancouver.<br/> <br/> I'm studying Computer Engineering at The University of British Columbia and I'm currently a Software Developer Intern at SAP.<br/> <br/>
-                  I've always been fascinated by technology and I want be a part of developing the software that continues to change human nature. I'm still finding my way around the industry but I have an interest in Backend Development and Embedded Systems and I have picked up some Frontend skills along the way.<br/> <br/> 
-                  Here are a few tools that I have been working with.  <br/> <br/>
+                  I've always been fascinated by technology and I want be a part of developing the software that continues to change human nature. I'm still finding my way around the industry but I have an interest in Backend Development and Embedded Systems and I have picked up some Frontend skills along the way.
                   </p>
 
+               </div>}
+
+               {this.state.aboutSelect === "Skills" && 
                   <div className="about-skills">
                      <p>
                         » Go <br/>
@@ -32,7 +52,7 @@ class About extends React.Component {
                      </p>
 
                   </div>
-               </div>
+               }
                </div>
             </div>
          </div>
