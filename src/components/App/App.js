cff75fa19bb8f9c963d0c9e1c62f0f86532b06f4
mpchow <mattchow918@gmail.com>
2021-01-05 01:41:17 -0800

    Routing to pages, mobile message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 
 import Experience from '../Experience/experience';
 import Intro from '../Intro/intro';
 import Projects from '../Projects/projects';
 import About from '../About/about';
+import Resume from '../Resume/resume';
 
 function App() {
-  return (
-    <div className="App">
-      <Intro/>
-      <About />
-      <Experience />
-      <Projects />
-      <div className="App-footer">
-        <p>Designed & Built with <span role="img" aria-label="heart">❤️</span> by Matthew Chow | 2020</p>
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+  if (isMobile) {
+    return (
+      <div>
+        Come back later for a mobile friendly version!
       </div>
-    </div>
-  );
+    );
+  }
+  else {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+            <Route path="/">
+              <div >
+                <Intro/>
+                <About />
+                <Experience />
+                <Projects />
+                <div className="App-footer">
+                  <p>Designed & Built with <span role="img" aria-label="heart">❤️</span> by Matthew Chow | 2020</p>
+                </div>
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
 }
 
 export default App;
